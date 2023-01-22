30ec0e523d61e280dd2a1b358abe8341b3682183
Krimothiazine <krimouv1995@gmail.com>
2023-01-22 10:55:24 +0100

    fix images bug

--- src/info.js
+++ src/info.js
@@ -1,3 +1,12 @@
+import couscousImg from './images/couscous.jpg';
+import zvitiImg from './images/zviti2.jpeg';
+import chakhchokhaImg from './images/chakhchoka.jpeg';
+import teaImg from './images/sahara-tea.jpg';
+import baklavaImg from './images/baklava.jpg';
+import mamaImg from './images/mama-camel.jpg';
+import papaImg from './images/papa-camel.jpg';
+import babyImg from './images/baby-camel.jpg';
+
 // Home page
 const contentObj = {
   title: 'Khaima Restaurant',
@@ -33,19 +42,19 @@ const mainDishes = {
     name: 'Couscous',
     desc: 'Couscous is a traditional North African dish made from small steamed balls of semolina wheat. It is often served as a side dish with meat or vegetables and can be made sweet or savory. It is a staple in Algerian cuisine and is enjoyed by locals and visitors alike.',
     price: '3$',
-    imgSrc: '../src/images/couscous.jpg', // Image by chefmouhcine on pixabay, https://pixabay.com/photos/couscous-vegetable-meat-recipe-3440042/
+    imgSrc: couscousImg, // Image by chefmouhcine on pixabay, https://pixabay.com/photos/couscous-vegetable-meat-recipe-3440042/
   },
   zviti: {
     name: 'Zviti',
     desc: 'Zviti is a traditional Algerian dish consisting of stewed lamb or beef, mixed with chickpeas and a variety of spices. It is often served with couscous and is a hearty, comforting meal. It is a staple of Algerian cuisine and is enjoyed by locals and visitors alike.',
     price: '4$',
-    imgSrc: '../src/images/zviti2.jpeg', // Image by Djazair Cooking on tweeter post, https://twitter.com/DjazairCooking/status/1519767427684438017/photo/1
+    imgSrc: zvitiImg, // Image by Djazair Cooking on tweeter post, https://twitter.com/DjazairCooking/status/1519767427684438017/photo/1
   },
   chakhchokha: {
     name: 'Chakhchokha',
     desc: 'Chakhchokha is a traditional Algerian dish made of cooked and mashed tomatoes and bell peppers, typically served with bread. It is a hearty and flavorful dish, and is often served as a side dish or as a topping for meat dishes. It is a staple in Algerian cuisine and is enjoyed by locals and visitors alike.',
     price: '3$',
-    imgSrc: '../src/images/chakhchoka.jpeg', // Image from https://www.myexcellentdegustations.com/algerian-thin-flatbread-with-meat-sauce-chakhchoukha/
+    imgSrc: chakhchokhaImg, // Image from https://www.myexcellentdegustations.com/algerian-thin-flatbread-with-meat-sauce-chakhchoukha/
   },
 };
 
@@ -55,13 +64,13 @@ const entrees = {
     name: 'Sahara Tea',
     desc: 'Algerian Sahara tea is a type of black tea that is grown and produced in the Sahara desert region of Algeria. It is known for its strong, bold flavor and is often served with mint or other herbs to enhance the taste. It is considered a traditional drink in Algeria and is enjoyed by many locals and tourists alike.',
     price: '0.5$',
-    imgSrc: '../src/images/sahara-tea.jpg', // Image by pirkerchri on pixabay, https://pixabay.com/images/id-2431459/
+    imgSrc: teaImg, // Image by pirkerchri on pixabay, https://pixabay.com/images/id-2431459/
   },
   baklava: {
     name: 'Baklava',
     desc: 'Algerian baklava is a sweet pastry made from layers of phyllo dough filled with chopped nuts, typically almonds or pistachios. It is sweetened with honey or syrup and is often flavored with cinnamon or orange blossom water. It is a popular dessert in Algeria and can be found in many bakeries and restaurants throughout the country.',
     price: '2$',
-    imgSrc: '../src/images/baklava.jpg', // Image by fabrikasimf on Freepik, https://www.freepik.com/author/fabrikasimf
+    imgSrc: baklavaImg, // Image by fabrikasimf on Freepik, https://www.freepik.com/author/fabrikasimf
   },
 };
 
@@ -74,21 +83,21 @@ const contacts = {
     job: 'Chef',
     phone: '548-545-524',
     email: 'mamaCamelAg@fake.com',
-    img: '../src/images/mama-camel.jpg',
+    img: mamaImg,
   },
   papCamel: {
     name: 'Papa Camel',
     job: 'Manager',
     phone: '442-544-111',
     email: 'papaCamelAg@fake.com',
-    img: '../src/images/papa-camel.jpg',
+    img: papaImg,
   },
   babyCamel: {
     name: 'Baby Camel',
     job: 'Waiter',
     phone: '542-444-200',
     email: 'babyCamelAg@fake.com',
-    img: '../src/images/baby-camel.jpg',
+    img: babyImg,
   },
 };
 
